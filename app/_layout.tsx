d01c5ead7cfcf602eb07d39cb2077596d55cae6e
Aly Mobarak <147055589+AlyMobarak@users.noreply.github.com>
2026-02-10 11:05:09 +0200

    Add automatic Expo update checking and prompting on app launch.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,9 @@
 import { DarkTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import * as Updates from "expo-updates";
+import { useEffect } from "react";
+import { Alert } from "react-native";
 import "react-native-reanimated";
 
 export const unstable_settings = {
@@ -8,6 +11,36 @@ export const unstable_settings = {
 };
 
 export default function RootLayout() {
+  async function onFetchUpdateAsync() {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        Alert.alert(
+          "Update Available",
+          "A new version of the app is available. Restart now to apply changes?",
+          [
+            {
+              text: "Wait",
+              style: "cancel",
+            },
+            { text: "Restart", onPress: () => Updates.reloadAsync() },
+          ],
+        );
+      }
+    } catch (error) {
+      // You can also add an alert() to see the error message in case of an error when fetching updates.
+      console.log(`Error fetching latest Expo update: ${error}`);
+    }
+  }
+
+  useEffect(() => {
+    if (!__DEV__) {
+      onFetchUpdateAsync();
+    }
+  }, []);
+
   return (
     <ThemeProvider value={DarkTheme}>
       <Stack>
